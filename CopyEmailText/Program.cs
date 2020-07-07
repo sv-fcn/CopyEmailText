d0addae09c1c6588e717259c759a79dd899362ed
Stefan Vestergaard <sv@fundconnect.com>
2020-07-07 08:18:16 +0200

    Moved options class

--- CopyEmailText/Program.cs
+++ CopyEmailText/Program.cs
@@ -40,8 +40,6 @@ namespace CopyEmailText
             }
         }
 
-
-
         private static ImapClient ImapConnect( string host, int port, string username, string password )
         {
             ComponentInfo.SetLicense( "MAAM-UCBJ-1F3O-WEDX" );
@@ -132,18 +130,5 @@ namespace CopyEmailText
 
             serviceCollection.AddSingleton( _configuration );
         }
-    }
-
-    public class SearchOptions
-    {
-        public string Host { get; set; }
-        public int Port { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string SearchFrom { get; set; }
-        public string SearchSubject { get; set; }
-        public int SearchNumberOfMessages { get; set; }
-        public int ShowConsoleSeconds { get; set; }
-
     }
 }
---
+++ CopyEmailText/SearchOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace CopyEmailText
+{
+    public class SearchOptions
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string SearchFrom { get; set; }
+        public string SearchSubject { get; set; }
+        public int SearchNumberOfMessages { get; set; }
+        public int ShowConsoleSeconds { get; set; }
+
+    }
+}
